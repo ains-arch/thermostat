74f5ec8678cfc011bfc64863d38c363affa1f636
ains-arch <ainsleearchibald@gmail.com>
2025-12-12 20:35:20 -0700

    helper executable for closing relays

---
+++ close_relays.cpp
@@ -0,0 +1,8 @@
+#include <gpiod.hpp>
+#include <print>
+#include "functions.h"
+
+int main() {
+    std::println("=== Turning Off Relays ===");
+    ThermostatState state; // run the constructor, init gpio
+};
